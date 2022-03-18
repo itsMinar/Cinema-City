06b82db7568d8691f795aa0b6008775a001a3799
Md. Minar Munshi <75926025+itsMinar@users.noreply.github.com>
2022-03-18 11:14:01 +0600

    Redesign the required Buttons
    Co-Authored-By: rireaz <100343232+rireaz@users.noreply.github.com>
    Co-Authored-By: Romjan Munchi <99871043+RomjanMunchi@users.noreply.github.com>

--- Cinema City/User Controls/User_Dashboard.Designer.cs
+++ Cinema City/User Controls/User_Dashboard.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Cinema_City.User_Controls
 {
-    partial class User_Dashboard
+    partial class user_Dashboard
     {
         /// <summary> 
         /// Required designer variable.
@@ -29,27 +29,167 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(user_Dashboard));
+            this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.datePanel = new System.Windows.Forms.Panel();
+            this.month = new System.Windows.Forms.Label();
+            this.date = new System.Windows.Forms.Label();
+            this.day = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.guna2Elipse2 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.guna2DragControl1 = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.guna2DragControl2 = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.panel1.SuspendLayout();
+            this.datePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
-            // bunifuElipse1
+            // guna2Elipse1
             // 
-            this.bunifuElipse1.ElipseRadius = 30;
-            this.bunifuElipse1.TargetControl = this;
+            this.guna2Elipse1.BorderRadius = 30;
+            this.guna2Elipse1.TargetControl = this;
             // 
-            // User_Dashboard
+            // panel1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.datePanel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(810, 177);
+            this.panel1.TabIndex = 0;
+            // 
+            // datePanel
+            // 
+            this.datePanel.BackColor = System.Drawing.Color.LightSalmon;
+            this.datePanel.Controls.Add(this.month);
+            this.datePanel.Controls.Add(this.date);
+            this.datePanel.Controls.Add(this.day);
+            this.datePanel.Controls.Add(this.pictureBox1);
+            this.datePanel.Location = new System.Drawing.Point(476, 15);
+            this.datePanel.Name = "datePanel";
+            this.datePanel.Size = new System.Drawing.Size(303, 148);
+            this.datePanel.TabIndex = 0;
+            // 
+            // month
+            // 
+            this.month.AutoSize = true;
+            this.month.Font = new System.Drawing.Font("Microsoft Tai Le", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.month.ForeColor = System.Drawing.Color.Transparent;
+            this.month.Location = new System.Drawing.Point(169, 104);
+            this.month.Name = "month";
+            this.month.Size = new System.Drawing.Size(90, 31);
+            this.month.TabIndex = 1;
+            this.month.Text = "Month";
+            // 
+            // date
+            // 
+            this.date.AutoSize = true;
+            this.date.Font = new System.Drawing.Font("Microsoft Tai Le", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.date.ForeColor = System.Drawing.Color.Transparent;
+            this.date.Location = new System.Drawing.Point(184, 54);
+            this.date.Name = "date";
+            this.date.Size = new System.Drawing.Size(88, 41);
+            this.date.TabIndex = 1;
+            this.date.Text = "Date";
+            // 
+            // day
+            // 
+            this.day.AutoSize = true;
+            this.day.Font = new System.Drawing.Font("Microsoft Tai Le", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.day.ForeColor = System.Drawing.Color.Transparent;
+            this.day.Location = new System.Drawing.Point(169, 14);
+            this.day.Name = "day";
+            this.day.Size = new System.Drawing.Size(58, 31);
+            this.day.TabIndex = 1;
+            this.day.Text = "Day";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(28, 14);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 121);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // guna2Elipse2
+            // 
+            this.guna2Elipse2.BorderRadius = 30;
+            this.guna2Elipse2.TargetControl = this.datePanel;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(51, 19);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(216, 148);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // guna2DragControl1
+            // 
+            this.guna2DragControl1.TargetControl = this.panel1;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 173);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(810, 437);
+            this.panel2.TabIndex = 1;
+            // 
+            // guna2DragControl2
+            // 
+            this.guna2DragControl2.TargetControl = this.panel2;
+            // 
+            // user_Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(52)))), ((int)(((byte)(52)))));
-            this.Name = "User_Dashboard";
-            this.Size = new System.Drawing.Size(874, 620);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(46)))), ((int)(((byte)(64)))));
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "user_Dashboard";
+            this.Size = new System.Drawing.Size(810, 610);
+            this.Load += new System.EventHandler(this.user_Dashboard_Load);
+            this.panel1.ResumeLayout(false);
+            this.datePanel.ResumeLayout(false);
+            this.datePanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel datePanel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label month;
+        private System.Windows.Forms.Label date;
+        private System.Windows.Forms.Label day;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel2;
+        private Guna.UI2.WinForms.Guna2DragControl guna2DragControl1;
+        private Guna.UI2.WinForms.Guna2DragControl guna2DragControl2;
     }
 }
--- Cinema City/User Controls/User_Dashboard.cs
+++ Cinema City/User Controls/User_Dashboard.cs
@@ -10,11 +10,23 @@ using System.Windows.Forms;
 
 namespace Cinema_City.User_Controls
 {
-    public partial class User_Dashboard : UserControl
+    public partial class user_Dashboard : UserControl
     {
-        public User_Dashboard()
+        public user_Dashboard()
         {
             InitializeComponent();
+        }
+
+        private void user_Dashboard_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            day.Text = DateTime.Now.ToString("dddd");
+            date.Text = DateTime.Now.ToString("dd");
+            month.Text = DateTime.Now.ToString("MMMM");
         }
     }
 }
